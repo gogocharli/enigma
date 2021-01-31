b42399b9c4653b57188c609d50bb005cd43188e8
Charles De Mount <thexrayone@icloud.com>
2021-01-30 22:07:00 -0500

    chore: update to major snowpack release 3.0

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,23 +1,17 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
+  packageOptions: {
     /* ... */
   },
   devOptions: {
     /* ... */
   },
   buildOptions: {
-    /* ... */
-  },
-  proxy: {
     /* ... */
   },
   alias: {
