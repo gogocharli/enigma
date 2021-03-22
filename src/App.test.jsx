f09a93972739a0a9c83551a971438fba207c959b
Charles De Mount <thexrayone@icloud.com>
2021-03-21 21:48:35 -0400

    test: :white_check_mark: add and setup jest

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
-import { expect } from 'chai';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
-  });
+test('loads correctly', () => {
+  render(<App />);
 });
