346873fa6afcb639db17b17358beeca7ca6d701a
Charles <thexrayone@icloud.com>
2022-07-13 15:56:26 -0400

    build: upgrade outdated plugins and move to vite 3.x

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {defineConfig} from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
 });
